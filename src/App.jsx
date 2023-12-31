87b2112876b33b8b5d69e79f59ae4e664f0b740e
crossroads30 <crossroads30@mail.ru>
2023-12-31 14:05:37 +0300

    feat: add title, add add static article with review

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
-import React, { useState, useEffect } from 'react';
-import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
-import { FaQuoteRight } from 'react-icons/fa';
-import data from './data';
+import React, { useState, useEffect } from 'react'
+import { FiChevronRight, FiChevronLeft } from 'react-icons/fi'
+import { FaQuoteRight } from 'react-icons/fa'
+import data from './data'
 function App() {
-  return <h2>slider project setup</h2>;
+	const [people, setPeople] = useState(data)
+	const [index, setIndex] = useState(0)
+	const { id, name, image, title, quote } = people[index]
+	return (
+		<main>
+			<section className='section'>
+				<div className='title'>
+					<h2>
+						<span>/</span>reviews
+					</h2>
+				</div>
+			</section>
+			<section className='section-center'>
+				<article>
+					<img className='person-img' src={image} alt={name} />
+					<h4>{name}</h4>
+					<div className='title'>{title}</div>
+					<p>{quote}</p>
+				</article>
+			</section>
+		</main>
+	)
 }
 
-export default App;
+export default App
