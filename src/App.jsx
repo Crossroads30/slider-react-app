e86f42702b3672476dd9d334fe37e1e2743d014f
crossroads30 <crossroads30@mail.ru>
2023-12-31 15:59:19 +0300

    feat: set up functionality for slides changing

--- src/App.jsx
+++ src/App.jsx
@@ -4,38 +4,69 @@ import { FaQuoteRight } from 'react-icons/fa'
 import data from './data'
 function App() {
 	const [people, setPeople] = useState(data)
-	const [index, setIndex] = useState(0)
+	const [index, setIndex] = useState(2)
+
+	const checkNumber = number => {
+		if (number < 0) {
+			return people.length - 1
+		}
+		if (number > people.length - 1) {
+			return 0
+		}
+		return number
+	}
+
+	const prevSlide = () => {
+		setIndex(index => {
+			const newIndex = index - 1
+			return checkNumber(newIndex)
+		})
+	}
+	const nextSlide = () => {
+		setIndex(index => {
+			const newIndex = index + 1
+			return checkNumber(newIndex)
+		})
+	}
 
 	return (
-		<main>
-			<section className='section'>
-				<div className='title'>
-					<h2>
-						<span>/</span>reviews
-					</h2>
-				</div>
-				<div className='section-center'>
-					{people.map((person, personIndex) => {
-						const { id, name, image, title, quote } = person
-						return (
-							<article key={id}>
-								<img className='person-img' src={image} alt={name} />
-								<h4>{name}</h4>
-								<p className='title'>{title}</p>
-								<p className='text'>{quote}</p>
-								<FaQuoteRight className='icon' />
-							</article>
-						)
-					})}
-					<button className='prev' type='button'>
-						<FiChevronLeft />
-					</button>
-					<button className='next' type='button'>
-						<FiChevronRight />
-					</button>
-				</div>
-			</section>
-		</main>
+		<section className='section'>
+			<div className='title'>
+				<h2>
+					<span>/</span>reviews
+				</h2>
+			</div>
+			<div className='section-center'>
+				{people.map((person, personIndex) => {
+					const { id, name, image, title, quote } = person
+					let position = 'nextSlide'
+					if (personIndex === index) {
+						position = 'activeSlide'
+					}
+					if (
+						personIndex === index - 1 ||
+						(index === 0 && personIndex === people.length - 1)
+					) {
+						position = 'lastSlide'
+					}
+					return (
+						<article className={position} key={id}>
+							<img className='person-img' src={image} alt={name} />
+							<h4>{name}</h4>
+							<p className='title'>{title}</p>
+							<p className='text'>{quote}</p>
+							<FaQuoteRight className='icon' />
+						</article>
+					)
+				})}
+				<button onClick={prevSlide} className='prev' type='button'>
+					<FiChevronLeft />
+				</button>
+				<button onClick={nextSlide} className='next' type='button'>
+					<FiChevronRight />
+				</button>
+			</div>
+		</section>
 	)
 }
 
